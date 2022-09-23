48ddb19a32cb851bb8d66aaa5e9c7d0ec084b98d
DiogoCarreiroRego <diogocarreirorego@enta.pt>
2022-09-23 21:46:24 +0000

    23-09-2022 21:46 Capítulo 2 Elementos Básicos de Programação 2º Exercício
    - Um programa que lê valores correspondentes a uma distância percorrida (em Km)
    e o tempo necessário para percorrer (em minutos), e calcular a velocidade média em:
        (a) Km / h
        (b) m / s

---
+++ programa_ex2.py
@@ -0,0 +1,40 @@
+"""
+Capítulo 2
+Elementos Básicos de Programação
+2º Exercício
+
+- Um programa que lê valores correspondentes a uma distância percorrida (em Km)
+e o tempo necessário para percorrer (em minutos), e calcular a velocidade média em:
+    (a) Km / h
+    (b) m / s
+"""
+
+
+def media_km(valor1, valor2):
+    valor2 = valor2 / 60
+    media = valor1 / valor2
+
+    return media
+
+
+def media_m(valor1, valor2):
+    valor1 = valor2 / 1000
+    valor2 = valor2 * 60
+    media = valor1 / valor2
+
+    return media
+
+
+if __name__ == '__main__':
+    nome = input('Como se chama? ')
+    while True:
+        distancia = int(input('Qual foi a distância percorrida (em Km)? '))
+        tempo = int(input('Qual é o tempo necessário a percorrer (em minutos)? '))
+
+        print(f'{distancia}(Km) : {tempo}(min) = {media_km(distancia, tempo)} km/h')
+        print(f'{distancia}(Km) : {tempo}(min) = {media_m(distancia, tempo)} m/s')
+
+        continuar = input('Repetir (s | n)? ')
+        if continuar == 'n':
+            break
+    print(f'Adeus {nome}!')
