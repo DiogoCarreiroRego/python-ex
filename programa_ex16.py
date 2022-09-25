3acd5986d82a3cbe01d419e1ffb4fb5269b2df1d
DiogoCarreiroRego <diogocarreirorego@enta.pt>
2022-09-25 13:39:15 +0000

    25-09-2022 13:39 Capítulo 2 Elementos Básicos de Programação 16º Exercício
    - Um programa que lê um número e crie a uma capicua cuja primeira metade é o número lido.
    Por exemplo:
        * Escreve um número
        ? 347
        347743

---
+++ programa_ex16.py
@@ -0,0 +1,30 @@
+"""
+Capítulo 2
+Elementos Básicos de Programação
+16º Exercício
+
+- Um programa que lê um número e crie a uma capicua cuja primeira metade é o número lido.
+Por exemplo:
+    * Escreve um número
+    ? 347
+    347743
+"""
+
+
+def numero(valor):
+    valor = str(valor)
+    numero = valor + valor[::-1]
+
+    return numero
+
+
+if __name__ == '__main__':
+    while True:
+        num = int(input('Qual é o número? '))
+
+        print(f'O número é {numero(num)}.')
+
+        continuar = input('Repetir (s | n)? ')
+        if continuar == 'n':
+            break
+    print(f'Adeus!')
