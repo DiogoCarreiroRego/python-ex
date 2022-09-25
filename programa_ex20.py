a4e9d62e7792ecbdb73ac435c5ecbe6f71f7d761
DiogoCarreiroRego <diogocarreirorego@enta.pt>
2022-09-25 11:25:28 +0000

    25-09-2022 11:25 Capítulo 2 Elementos Básicos de Programação 6º Exercício
    - Um programa que lê três números e que diz qual o maior dos números lidos.

---
+++ programa_ex20.py
@@ -0,0 +1,30 @@
+"""
+Capítulo 2
+Elementos Básicos de Programação
+20º Exercício
+
+- Um programa que escreve o seguinte:
+    1 × 8 + 1 = 9
+    12 × 8 + 2 = 98
+    123
+"""
+
+
+"""
+def dia(valor):
+    dias = valor / 60
+    dias = dias / 60
+    dias = dias / 24
+
+    return dias
+
+
+if __name__ == '__main__':
+    while True:
+        segundos = int(input('Quantos segundos? '))
+
+        continuar = input('Repetir (s | n)? ')
+        if continuar == 'n':
+            break
+    print(f'Adeus!')
+"""
