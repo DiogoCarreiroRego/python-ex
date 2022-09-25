17ec0c2de6c8adbda0124249f7c142ee3d205af3
DiogoCarreiroRego <diogocarreirorego@enta.pt>
2022-09-25 20:40:19 +0000

    25-09-2022 20:40 Capítulo 2 Elementos Básicos de Programação 19º Exercício
    - Um programa que lê uma quantidade em Euros e calcula o número de nota de 50€ a 5€ e
    moedas de 2€ a 1€ e cêncimos de 0.5€ a 0.01€

--- programa_ex19.py
+++ programa_ex19.py
@@ -3,7 +3,8 @@ Capítulo 2
 Elementos Básicos de Programação
 19º Exercício
 
-- Um programa que
+- Um programa que lê uma quantidade em Euros e calcula o número de nota de 50€ a 5€ e
+moedas de 2€ a 1€ e cêncimos de 0.5€ a 0.01€
 """
 
 
@@ -17,12 +18,12 @@ def nota(valor):
     moedas_1 = int((valor - (notas_50 * 50) - (notas_20 * 20) - (notas_10 * 10) - (moedas_5 * 5) - (moedas_2 * 2)) // 1)
     centimos__50 = int((valor - (notas_50 * 50) - (notas_20 * 20) - (notas_10 * 10) - (moedas_5 * 5) - (moedas_2 * 2) - (moedas_1 * 1)) // 0.5)
     centimos__20 = int((valor - (notas_50 * 50) - (notas_20 * 20) - (notas_10 * 10) - (moedas_5 * 5) - (moedas_2 * 2) - (moedas_1 * 1) - (centimos__50 * 0.5)) // 0.2)
-    centimos__10 = int((valor - (notas_50 * 50) - (notas_20 * 20) - (notas_10 * 10) - (moedas_5 * 5) - (moedas_2 * 2) - (moedas_1 * 1) - (centimos__50 * 0.5) - (centimos__20 * 0.2)) / 0.1)
-    centimos__5 = ''
-    centimos__2 = ''
-    centimos__1 = ''
+    centimos__10 = int((valor - (notas_50 * 50) - (notas_20 * 20) - (notas_10 * 10) - (moedas_5 * 5) - (moedas_2 * 2) - (moedas_1 * 1) - (centimos__50 * 0.5) - (centimos__20 * 0.2)) // 0.1)
+    centimos__5 = int((valor - (notas_50 * 50) - (notas_20 * 20) - (notas_10 * 10) - (moedas_5 * 5) - (moedas_2 * 2) - (moedas_1 * 1) - (centimos__50 * 0.5) - (centimos__20 * 0.2) - (centimos__10 * 0.1)) // 0.05)
+    centimos__2 = int((valor - (notas_50 * 50) - (notas_20 * 20) - (notas_10 * 10) - (moedas_5 * 5) - (moedas_2 * 2) - (moedas_1 * 1) - (centimos__50 * 0.5) - (centimos__20 * 0.2) - (centimos__10 * 0.1) - (centimos__5 * 0.05)) // 0.02)
+    centimos__1 = int((valor - (notas_50 * 50) - (notas_20 * 20) - (notas_10 * 10) - (moedas_5 * 5) - (moedas_2 * 2) - (moedas_1 * 1) - (centimos__50 * 0.5) - (centimos__20 * 0.2) - (centimos__10 * 0.1) - (centimos__5 * 0.05) - (centimos__2 * 0.02)) // 0.01)
     print(f'[{notas_50}]50€ [{notas_20}]20€ [{notas_10}]10€ [{moedas_5}]5€ [{moedas_2}]2€ [{moedas_1}]1€')
-    print(f'[{centimos__50}]0.5€ [{centimos__20}]0.2€ [{centimos__10}]0.1€ [{moedas_5}]5€ [{moedas_2}]2€ [{moedas_1}]1€')
+    print(f'[{centimos__50}]0.5€ [{centimos__20}]0.2€ [{centimos__10}]0.1€ [{centimos__5}]0.05€ [{centimos__2}]0.02€ [{centimos__1}]0.01€')
 
 
 if __name__ == '__main__':
