ce1961b505837609bc84ad3793899ba58d4e3ff6
DiogoCarreiroRego <diogocarreirorego@enta.pt>
2022-10-03 08:53:52 +0000

    03-10-2022 08:51 Capítulo 2 Elementos Básicos de Programação 2º Exercício
    - Um programa que lê valores correspondentes a uma distância percorrida (em Km)
    e o tempo necessário para percorrer (em minutos), e calcular a velocidade média em:
        (a) Km / h
        (b) m / s
    * Calcular a velocidade média

--- programa_ex2.py
+++ programa_ex2.py
@@ -27,15 +27,19 @@ def media_m(valor1, valor2):
 
 
 if __name__ == '__main__':
-    nome = input('Como se chama? ')
     while True:
-        distancia = int(input('Qual foi a distância percorrida (em Km)? '))
-        tempo = int(input('Qual é o tempo necessário a percorrer (em minutos)? '))
+        try:
+            distancia = int(input('Qual foi a distância percorrida (em Km)? '))
+            tempo = int(input('Qual é o tempo necessário a percorrer (em minutos)? '))
 
-        print(f'{distancia}(Km) : {tempo}(min) = {media_km(distancia, tempo)} km/h')
-        print(f'{distancia}(Km) : {tempo}(min) = {media_m(distancia, tempo)} m/s')
+            print(f'{distancia}(Km) : {tempo}(min) = {media_km(distancia, tempo)} km/h')
+            print(f'{distancia}(Km) : {tempo}(min) = {media_m(distancia, tempo)} m/s')
 
-        continuar = input('Repetir (s | n)? ')
-        if continuar == 'n':
-            break
-    print(f'Adeus {nome}!')
+            continuar = input('Repetir (s | n)? ')
+            if continuar == 'n':
+                break
+
+        except ValueError:
+            print('Digite um valor válido')
+
+    print(f'Adeus!')
---
+++ programa_ex5.py
@@ -0,0 +1,13 @@
+"""
+Capítulo 2
+Elementos Básicos de Programação
+5º Exercício
+
+- Um programa que
+"""
+
+from math import sqrt
+
+
+if __name__ == '__main__':
+    print(sqrt(4))
