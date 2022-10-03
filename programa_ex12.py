6e69cf49cb2642a5a439e2c6ebf66efd0857456d
DiogoCarreiroRego <diogocarreirorego@enta.pt>
2022-10-03 12:12:38 +0000

    03-10-2022 11:50 Capítulo 2 Elementos Básicos de Programação 12º Exercício
    - Um programa que

--- programa_ex12.py
+++ programa_ex12.py
@@ -1,7 +1,7 @@
 """
 Capítulo 2
 Elementos Básicos de Programação
-10º Exercício
+12º Exercício
 
 - Um programa que
 """
@@ -12,6 +12,12 @@ if __name__ == '__main__':
         try:
             x = int(input('Qual o valor de x: '))
             n = int(input('Qual o valor de n: '))
+
+            soma = 1 + x
+            for z in range(2, n + 1):
+                soma += (x ** z) / z
+
+            print(soma)
 
             continuar = input('Repetir (s | n)? ')
             if continuar == 'n':
